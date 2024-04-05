b8b21b3f8ee67c0371704c0bcd0c0e54dfd8d85b
MarcoChavezB <marco1102004@gmail.com>
2024-04-04 21:03:10 -0600

    cambios

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import paho.mqtt.client as mqtt
+import time
 
 # Configura los parámetros de conexión MQTT
 MQTT_BROKER = "test.mosquitto.org"
@@ -7,20 +8,26 @@ MQTT_TOPIC = "led_control"
 
 # Función para manejar los mensajes MQTT
 def on_message(client, userdata, message):
-    payload = message.payload.decode("utaf-8")
+    payload = message.payload.decode("utf-8")
     print("Mensaje recibido:", payload)
 
 # Configura el cliente MQTT
 client = mqtt.Client()
 client.on_message = on_message
-client.connect(MQTT_BROKER, MQTT_PORT, 60)
-client.subscribe(MQTT_TOPIC)
-client.loop_start()
 
-# Bucle principal
+# Conexión al broker MQTT y suscripción al topic
 try:
+    client.connect(MQTT_BROKER, MQTT_PORT, 60)
+    client.subscribe(MQTT_TOPIC)
+    client.loop_start()
+    print("Conectado al broker MQTT. Esperando mensajes...")
+    
+    # Bucle principal
     while True:
         time.sleep(1)
+
 except KeyboardInterrupt:
+    print("Deteniendo el cliente MQTT...")
     client.disconnect()
     client.loop_stop()
+
